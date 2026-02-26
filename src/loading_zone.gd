ad1a96b2ff77ca434408b9a7b4bcce34fabe4eae
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-02-25 21:29:57 -0500

    Loading Zone moved to it's own scene

--- src/loading_zone.gd
+++ src/loading_zone.gd
@@ -1,9 +1,16 @@
+@tool
 extends Area2D
+@export var SceneID: int;
+@export var Size: Vector2 = Vector2(20,20):
+	set(value):
+		Size = value;
+		$CollisionShape2D.shape.size = value;
 signal switch_scene(id: int);
+
+var previouslyOverlapped = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(get_tree().current_scene)
 	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"):
 		switch_scene.connect(get_tree().current_scene._switch_scene)
 	pass # Replace with function body.
@@ -14,5 +21,5 @@ func _process(delta: float) -> void:
 	# Lambda function here for checking if the area has collided with a player.
 	if get_overlapping_bodies().any(func(obj): return obj is CharacterBody2D):
 		print("Collided with player")
-		switch_scene.emit(2);
+		switch_scene.emit(SceneID);
 	pass
