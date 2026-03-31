c6b4514d77aafe6973ee223328d6c66bb306f595
Tim Reiss <r223@umbc.edu>
2026-03-31 19:16:46 -0400

    create mandrake minigame

---
+++ src/Mandrake Minigame/mandrake_minigame.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+#spawn random mandrakes
+#they randomly activate and move around
+#click to knock them out
+#after they all leave, end game
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
