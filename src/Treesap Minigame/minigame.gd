4f7636ed991922c9d0dc1f104348f4128d21b750
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-03-31 19:01:02 -0400

    Tap minigame respond to nodes and no overlapping spots

--- src/Treesap Minigame/minigame.gd
+++ src/Treesap Minigame/minigame.gd
@@ -10,11 +10,17 @@ func _ready():
 	ChangeIngredients.connect(GameInfo._change_ingredient_amount);
 	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"):
 		ReturnToOverworld.connect(get_tree().current_scene._switch_scene);
+	var spawnedSpots: Array[Node2D] = [];
 	for i: int in numberOfTapSpots:
-		var tapInstance = tapSpot.instantiate();
-		tapInstance.position = Vector2(randi_range(-160, 160), randi_range(-288, 288));
+		var tapInstance: Node2D = tapSpot.instantiate();
+		var pos = Vector2(randi_range(-160, 160), randi_range(-288, 288));
+		while !spawnedSpots.filter(func(spot): return (spot.position - pos).length() < 128).is_empty():
+			pos = Vector2(randi_range(-160, 160), randi_range(-288, 288));
+		tapInstance.position = pos;
 		tapInstance.name = "Tap" + str(i);
+		tapInstance.add_to_group("Tree Tap Spot");
 		add_child(tapInstance);
+		spawnedSpots.append(tapInstance);
 	pass # Replace with function body.
 
 
--- src/Treesap Minigame/tap_control.gd
+++ src/Treesap Minigame/tap_control.gd
@@ -9,4 +9,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.position = lerp(self.position, get_global_mouse_position(), 0.2);
+	
+	var closestSpot: Node2D = null;
+	for child in get_parent().get_children().filter(func(spot): return spot.is_in_group("Tree Tap Spot")):
+		if closestSpot == null or (child.position - self.position).length() < (closestSpot.position - self.position).length():
+			closestSpot = child;
+	if closestSpot == null:
+		return
+	var closeness = 1.0 - clamp((self.position - closestSpot.position).length() / 200.0, 0.0, 1.0);
+	$AnimatedSprite2D.material.set_shader_parameter("closeness", closeness);
 	pass
