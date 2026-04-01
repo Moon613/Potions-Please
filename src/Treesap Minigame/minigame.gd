391c57ca89660971fa2bd3d346490245de3c1000
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-04-01 19:19:14 -0400

    Placing tap spots

--- src/Treesap Minigame/minigame.gd
+++ src/Treesap Minigame/minigame.gd
@@ -2,14 +2,29 @@ extends Node2D
 
 signal ReturnToOverworld(id: int);
 signal ChangeIngredients(ingr: String, amt: int);
+signal DrainTaps;
 @export var numberOfTapSpots: int = 4;
 var tapSpot: PackedScene = preload("res://Treesap Minigame/Tap Spot.tscn");
+var tap: PackedScene = preload("res://Treesap Minigame/tap.tscn");
+var tapsPlaced: int = 0;
+var resourceAmountCollected: int = 0;
+var tapsFinishedCollecting: int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ChangeIngredients.connect(GameInfo._change_ingredient_amount);
 	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"):
 		ReturnToOverworld.connect(get_tree().current_scene._switch_scene);
+	PlaceTapSpots();
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if tapsPlaced >= numberOfTapSpots:
+		DrainTaps.emit();
+
+func PlaceTapSpots():
 	var spawnedSpots: Array[Node2D] = [];
 	for i: int in numberOfTapSpots:
 		var tapInstance: Node2D = tapSpot.instantiate();
@@ -21,16 +36,35 @@ func _ready():
 		tapInstance.add_to_group("Tree Tap Spot");
 		add_child(tapInstance);
 		spawnedSpots.append(tapInstance);
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func Reset():
+	for child in get_children().filter(func(obj: Node2D): return obj.is_in_group("Tap") or obj.is_in_group("Tree Tap Spot")):
+		child.queue_free();
+	tapsPlaced = 0;
+	resourceAmountCollected = 0;
+	tapsFinishedCollecting = 0;
+	PlaceTapSpots();
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		ReturnToOverworld.emit(0);
+
+func _on_tap_control_place_tap(placedTapSpot: Vector2) -> void:
+	var tapInstance = tap.instantiate();
+	tapInstance.add_to_group("Tap");
+	tapInstance.set_script(load("res://Treesap Minigame/tap.gd"))
+	tapInstance.position = placedTapSpot;
+	DrainTaps.connect(tapInstance._on_drain_taps);
+	add_child(tapInstance);
+	tapsPlaced += 1;
+	var closestTapSpot: Node2D = null;
+	for child in get_children().filter(func(c): return c.is_in_group("Tree Tap Spot")):
+		if closestTapSpot == null or (child.position - placedTapSpot).length() < (closestTapSpot.position - placedTapSpot).length():
+			closestTapSpot = child;
+	closestTapSpot.add_to_group("Already Tapped");
+	tapInstance.closestNodePos = closestTapSpot.position;
+
+func _on_tap_sap_collection(resourceAmount: int):
+	tapsFinishedCollecting += 1;
+	resourceAmountCollected += resourceAmount;
--- src/Treesap Minigame/tap_control.gd
+++ src/Treesap Minigame/tap_control.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+signal PlaceTap(spot: Node2D);
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,11 +12,26 @@ func _process(delta):
 	self.position = lerp(self.position, get_global_mouse_position(), 0.2);
 	
 	var closestSpot: Node2D = null;
-	for child in get_parent().get_children().filter(func(spot): return spot.is_in_group("Tree Tap Spot")):
+	for child in get_parent().get_children().filter(func(spot): return spot.is_in_group("Tree Tap Spot") and !spot.is_in_group("Already Tapped")):
 		if closestSpot == null or (child.position - self.position).length() < (closestSpot.position - self.position).length():
 			closestSpot = child;
 	if closestSpot == null:
 		return
 	var closeness = 1.0 - clamp((self.position - closestSpot.position).length() / 200.0, 0.0, 1.0);
 	$AnimatedSprite2D.material.set_shader_parameter("closeness", closeness);
+	
+	if closeness >= 0.5 and $AnimatedSprite2D.animation == "Far":
+		var frameBefore = $AnimatedSprite2D.frame;
+		var progress = $AnimatedSprite2D.frame_progress;
+		$AnimatedSprite2D.animation = "Close";
+		$AnimatedSprite2D.set_frame_and_progress(frameBefore, progress);
+	elif closeness < 0.5 and $AnimatedSprite2D.animation == "Close":
+		var frameBefore = clamp($AnimatedSprite2D.frame, 0, 1);
+		var progress = $AnimatedSprite2D.frame_progress;
+		$AnimatedSprite2D.animation = "Far";
+		$AnimatedSprite2D.set_frame_and_progress(frameBefore, progress);
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+		PlaceTap.emit(self.position);
