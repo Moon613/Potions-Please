bdf9cf722a7ce6496ac55b8b85310bbb75177c35
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-04-02 18:48:24 -0400

    Dragon Egg minigame spotlight and loadzone

---
+++ src/Dragon Egg Minigame/minigame.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+signal ReturnToOverworld(id: int);
+signal ChangeIngredients(ingr: String, amt: int);
+
+@export var maxTime: int = 500;
+var timer: float = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ChangeIngredients.connect(GameInfo._change_ingredient_amount);
+	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"):
+		ReturnToOverworld.connect(get_tree().current_scene._switch_scene);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$ColorRect.material.set_shader_parameter("mousePos", get_global_mouse_position() / -(get_viewport_rect().size / $Camera2D.zoom));
+	if timer >= maxTime:
+		pass
+	timer += 1*delta;
+	pass
+
+func Reset():
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
+		ReturnToOverworld.emit(0);
