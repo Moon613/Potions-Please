555ddb9e76a0b172bb24deca05cb48cd72b36111
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-04-14 19:21:18 -0400

    Start sap feedback

---
+++ src/Treesap Minigame/rating.gd
@@ -0,0 +1,55 @@
+extends AnimatedSprite2D
+
+var startingPos: Vector2;
+var appearing: bool = true;
+var waiting: bool = false;
+var timer: float = 0.0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	startingPos = self.position;
+	TweenRotate(0.1, 0.05);
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if appearing:
+		timer += delta;
+		self.scale *= 1.1;
+		if timer >= 0.2:
+			appearing = false;
+			waiting = true;
+			self.timer = 0;
+	if waiting:
+		self.timer += delta;
+		if self.timer >= 0.5:
+			self.rotation = 0;
+			self.timer = 0.2;
+			self.waiting = false;
+	if !appearing and !waiting:
+		self.scale *= 0.75;
+		self.timer -= delta;
+	self.position.y -= 0.5;
+	if self.timer <= 0:
+		self.queue_free();
+
+
+func SetSprite(id: int):
+	self.frame = id;
+
+
+func TweenRotate(endRotation: float, speed: float):
+	var tween = get_tree().create_tween();
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
+	tween.tween_property(self, "rotation", endRotation*-1.0, speed);
+	tween.tween_property(self, "rotation", endRotation, speed);
--- src/Treesap Minigame/tap_control.gd
+++ src/Treesap Minigame/tap_control.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 signal PlaceTap(spot: Node2D);
 var canPlaceMoretaps: bool = true;
+var currentCloseness: float = 0;
+var rating: PackedScene = preload("res://Treesap Minigame/rating.tscn");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,24 +20,30 @@ func _process(delta):
 			closestSpot = child;
 	if closestSpot == null:
 		return
-	var closeness = 1.0 - clamp((self.position - closestSpot.position).length() / 200.0, 0.0, 1.0);
-	$AnimatedSprite2D.material.set_shader_parameter("closeness", closeness);
+	currentCloseness = 1.0 - clamp((self.position - closestSpot.position).length() / 200.0, 0.0, 1.0);
+	$AnimatedSprite2D.material.set_shader_parameter("closeness", currentCloseness);
 	
-	if closeness >= 0.7 and $AnimatedSprite2D.animation == "Far":
+	# These are for flipping between the far and close animations while preserving frame progress.
+	if currentCloseness >= 0.7 and $AnimatedSprite2D.animation == "Far":
 		var frameBefore = $AnimatedSprite2D.frame;
 		var progress = $AnimatedSprite2D.frame_progress;
 		$AnimatedSprite2D.animation = "Close";
 		$AnimatedSprite2D.set_frame_and_progress(frameBefore, progress);
-	elif closeness < 0.7 and $AnimatedSprite2D.animation == "Close":
+	elif currentCloseness < 0.7 and $AnimatedSprite2D.animation == "Close":
 		var frameBefore = clamp($AnimatedSprite2D.frame, 0, 1);
 		var progress = $AnimatedSprite2D.frame_progress;
 		$AnimatedSprite2D.animation = "Far";
 		$AnimatedSprite2D.set_frame_and_progress(frameBefore, progress);
-	pass
+	
 
 func _input(event: InputEvent) -> void:
 	if canPlaceMoretaps and event is InputEventMouseButton and event.button_index == 1 and event.pressed:
 		PlaceTap.emit(self.position);
+		var ratingInstance = rating.instantiate();
+		if currentCloseness < 0.25:
+			ratingInstance.SetSprite(0);
+		ratingInstance.position = self.position + Vector2(0,-40);
+		get_parent().add_child(ratingInstance);
 
 func _on_minigame_drain_taps() -> void:
 	canPlaceMoretaps = false;
