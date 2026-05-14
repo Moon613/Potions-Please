b03cb38f127a4ea3274dd0479b1053cea7f9beeb
REDOGRE\Tim <timreis256@gmail.com>
2026-05-14 16:21:15 -0400

    added ambience, added waterfall sound effect, added a pause before overworld music loops

--- src/Music/bg_music.gd
+++ src/Music/bg_music.gd
@@ -1,6 +1,10 @@
 extends AudioStreamPlayer
 
-const OVERWORLD = preload("uid://da1nxj0mh08f1")
+const OVERWORLD_MUSIC = preload("uid://da1nxj0mh08f1")
+@onready var ambience: AudioStreamPlayer = $ambience
+const fall_ambience = preload("uid://c3ut0mj7lnff1")
+@onready var loop_timer: Timer = $LoopTimer
+var currentScene: int
 
 var sceneID
 # Called when the node enters the scene tree for the first time.
@@ -13,21 +17,42 @@ func _process(delta: float) -> void:
 	pass
 	
 func _on_scene_change(oldScene: int, newScene: int):
+	currentScene = newScene
 	if (newScene == GameInfo.SceneID.MAINMENU):
 		stop()
+		ambience.stop()
 	elif (oldScene == GameInfo.SceneID.MAINMENU):
-		stream = OVERWORLD
+		stream = OVERWORLD_MUSIC
 		play()
+		ambience.stream = fall_ambience
+		ambience.play()
 	if (newScene == GameInfo.SceneID.UPSTAIRS):
 		set_bus("ExtraMuffled")
+		ambience.set_bus("ExtraMuffled")
 	if (newScene == GameInfo.SceneID.INSIDEHOUSE or newScene == GameInfo.SceneID.POTIONBREWING):
 		set_bus("Muffled")
+		ambience.set_bus("Muffled")
 	if (newScene == GameInfo.SceneID.OVERWORLD):
 		set_bus("Master")
+		ambience.set_bus("Master")
 	if (newScene == GameInfo.SceneID.MANDRAKES or 
 		newScene == GameInfo.SceneID.DEWDROPS or 
 		newScene == GameInfo.SceneID.ACORNS or 
 		newScene == GameInfo.SceneID.TREESAP):
 			set_bus("Minigame")
+			ambience.set_bus("Minigame")
 	if (newScene == GameInfo.SceneID.DRAGONEGGS):
 		set_bus("Cave")
+		ambience.set_bus("Cave")
+	
+
+
+func _on_finished() -> void:
+	loop_timer.start()
+	pass # Replace with function body.
+
+
+func _on_loop_timer_timeout() -> void:
+	if currentScene != GameInfo.SceneID.MAINMENU:
+		play()
+	pass # Replace with function body.
