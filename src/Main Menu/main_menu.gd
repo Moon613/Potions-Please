b63caa84580c0a7f313d15b3163ed5128d47fa5f
LOLZ1189 <amosko2@umbc.edu>
2026-05-14 13:17:32 -0400

    Title screen music

--- src/Main Menu/main_menu.gd
+++ src/Main Menu/main_menu.gd
@@ -1,11 +1,12 @@
 extends Control
+
 signal tempSignal(int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"): 
+	if get_tree().current_scene and get_tree().current_scene.has_method("_switch_scene"):
 		tempSignal.connect(get_tree().current_scene._switch_scene)
-
+	$AudioStreamPlayer.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,17 +16,15 @@ func _process(delta: float) -> void:
 		$"Load Game".disabled = false
 	pass
 
-
 func _on_new_game_pressed() -> void:
+	$AudioStreamPlayer.stop()
 	tempSignal.emit(GameInfo.SceneID.UPSTAIRS);
 	DialogueManager.IntroDialogue();
-
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_load_game_pressed() -> void:
+	$AudioStreamPlayer.stop()
 	SaveManager.load_game(0)
 	tempSignal.emit(2)
