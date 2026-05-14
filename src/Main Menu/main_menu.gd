5f07cd357dcf360b1b7cde86ba53f2585cfbff7d
REDOGRE\Tim <timreis256@gmail.com>
2026-05-14 14:40:55 -0400

    actually disabled load game button lmao

--- src/Main Menu/main_menu.gd
+++ src/Main Menu/main_menu.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not FileAccess.file_exists("user://SaveFile0.save"):
-		$"Load Game".disabled = true
-	else:
-		$"Load Game".disabled = false
+	#if not FileAccess.file_exists("user://SaveFile0.save"):
+		#$"Load Game".disabled = true
+	#else:
+		#$"Load Game".disabled = false
 	pass
 
 func _on_new_game_pressed() -> void:
