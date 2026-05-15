cac91b2bf2bdd8db4eb1c797ecc5fb8360df4f5b
Moon613 <68190337+Moon613@users.noreply.github.com>
2026-05-15 17:25:42 -0400

    Credits implemented

--- src/Main Menu/credits.gd
+++ src/Main Menu/credits.gd
@@ -1,6 +1,17 @@
 extends Control
 
-var creditsLabel: PackedScene = preload("res://Main Menu/CreditsText.tscn")
+var creditsLabel: PackedScene = preload("res://Main Menu/CreditsText.tscn");
+var fadeInTimer: float = 0;
+var show: bool = false;
+var credits: Array[String] = [
+	"Alexander Mosko",
+	"Elijah Manning",
+	"Megan Gregory",
+	"Mino Yevdayev",
+	"Priscilia Herrera",
+	"Tim Reiss",
+	"William Zendgraft"
+];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,13 +20,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if show:
+		self.modulate.a = clamp(modulate.a + delta*2, 0, 1);
+		
+		if self.modulate.a >= 1:
+			ShowNames();
+			show = false;
+	
+	if !show and !self.get_children().any(func(child): return child.is_in_group("Credits Text")):
+		self.modulate.a = clamp(modulate.a - delta*2, 0, 1);
+		self.mouse_filter = Control.MOUSE_FILTER_IGNORE;
 
+func ShowNames():
+	$Splash.emitting = true;
+	for i in credits.size():
+		var c: CreditsText = creditsLabel.instantiate();
+		c.Setup(-1 if i%2==0 else 1, get_viewport_rect(), credits[i]);
+		c.add_to_group("Credits Text");
+		add_child(c);
+		if i == credits.size()-1:
+			$Splash.emitting = false;
+		await get_tree().create_timer(2.0).timeout;
 
 func _on_main_menu_credits():
-	visible = true;
-	var c: CreditsText = creditsLabel.instantiate();
-	c.direction = 1;
-	c.pivot_offset = c.size * 0.5;
-	c.start = true;
-	add_child(c);
+	self.mouse_filter = Control.MOUSE_FILTER_STOP;
+	show = true;
--- src/Main Menu/credits_text.gd
+++ src/Main Menu/credits_text.gd
@@ -6,9 +6,12 @@ var direction: int = 0;
 var upTimer: float = 0;
 var startPos: Vector2 = Vector2.ZERO;
 var endPos: Vector2 = Vector2.ZERO;
-var doneRotating: bool = false;
 var sittingTimer: float = 0;
-var goingDown: bool = false;
+var goingUp: bool = false;
+var startRotation: float = 0;
+var flyAwayTimer: float = 0;
+const XDist: int = 400;
+const SIT_TIME: int = 2;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,31 +21,38 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if start:
-		if (endPos - position).length() <= 10:
-			doneRotating = true;
-		
-		if !goingDown:
+		if !goingUp and sittingTimer == 0:
 			startPos = Vector2(get_viewport_rect().end.x/2, get_viewport_rect().end.y);
-			endPos = Vector2(300*direction, 324.0);
+			self.visible = true;
 			upTimer += delta;
+			var x1 = sin(((upTimer/2) * PI) / 2);
+			var x2 = 1 - pow(1 - (upTimer/2), 4);
+			self.rotation_degrees = lerpf(startRotation, 0.0, clampf(x1, 0.0, 1.0));
+			self.position = Vector2(
+				lerpf(startPos.x, endPos.x, clampf(x1, 0.0, 1.0)),
+				lerpf(startPos.y, endPos.y, clampf(x2, 0.0, 1.0)));
 		
-			if !doneRotating:
-				self.rotation += 5 * delta * direction;
-			if doneRotating:
-				if abs(int(rotation_degrees))%360 <= 10:
-					self.rotation = 0;
-				else:
-					self.rotation += 5 * delta * direction;
-		
-		self.position = Vector2(
-			lerpf(startPos.x, endPos.x, clampf(upTimer/2, 0.0, 1.0)),
-			lerpf(startPos.y, endPos.y, clampf(upTimer/2, 0.0, 1.0)));
-		
-		if upTimer >= 1:
+		if upTimer/2 >= 1:
 			sittingTimer += delta;
 		
-		if sittingTimer >= 3 and !goingDown:
-			goingDown = true;
+		if sittingTimer >= SIT_TIME and !goingUp:
+			goingUp = true;
+			startPos = self.position;
 		
-		if goingDown:
-			self.position = position.lerp(Vector2(400*direction, 0), 0.1);
+		if goingUp:
+			flyAwayTimer += delta;
+			var x: float = flyAwayTimer/0.75;
+			self.position.y = lerpf(startPos.y, -50, 0 if x == 0 else pow(2, 10*x-10));
+			self.position.x = lerpf(startPos.x, startPos.x + 75*-direction, 1 if x == 1 else 1 - pow(2, -10*x));
+			self.rotation_degrees = lerp(0, 90*direction, 0 if x == 0 else pow(2, 10*x-10));
+			if self.position.y <= -50:
+				queue_free();
+
+func Setup(direction: int, screenRect: Rect2, text: String):
+	self.text = "[center]"+text+"[/center]";
+	const TIMES_TO_SPIN: int = 2;
+	self.direction = direction;
+	self.start = true;
+	startRotation = 360 * TIMES_TO_SPIN * direction;
+	endPos = Vector2(screenRect.size.x/2 - self.size.x/2 + XDist*direction, screenRect.size.y/2);
+	self.position = Vector2(get_viewport_rect().end.x/2, get_viewport_rect().end.y);
